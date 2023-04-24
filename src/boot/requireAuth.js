380908451986bd682cfd663952d5c773e83258c0
Thanaphat Kongngoen <tnpkng@gmail.com>
2023-04-24 17:24:29 +0700

    Update 24/4/66 last

--- src/boot/requireAuth.js
+++ src/boot/requireAuth.js
@@ -1,11 +1,16 @@
 import { boot } from "quasar/wrappers";
 import { LocalStorage } from "quasar";
 import { authenKey } from "src/boot/utils/config";
+import { teacherKey } from "src/boot/utils/config";
+
 export default boot(({ router }) => {
   router.beforeEach((to, from, next) => {
     // console.log("from", from, "to", to);
     if (to.matched.some((record) => record.meta.requireAuth)) {
-      if (LocalStorage.getItem(authenKey) != null) {
+      if (
+        LocalStorage.getItem(authenKey) != null &&
+        LocalStorage.getItem(teacherKey) != null
+      ) {
         next();
       } else {
         next({
